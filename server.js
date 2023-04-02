48f740b8077a2f868977c9ecb0ede81c4e60f0cf
roody1901 <79497806+roody1901@users.noreply.github.com>
2023-04-02 22:01:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,11 @@ const fs = require('fs')
 const bodyParser = require('body-parser')
 const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
-const clone = require('clone')
-const data = require('./users.json')
 
-const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
 let userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'users.json', {
-    _isFake: isProductionEnv
-})
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-
-server.use((req, res, next) => {
-    if (req.path !== '/')
-        router.user.setState(clone(data))
-    next()
-})
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
@@ -193,5 +178,3 @@ server.listen(9000, () => {
   console.log('Auth Server is Running on Port 9000...');
 })
 
-
-module.exports = server
